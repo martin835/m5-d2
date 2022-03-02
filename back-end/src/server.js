ad9542cde075b8a217980219b4674cffffc76b5e
martin835 <64438132+martin835@users.noreply.github.com>
2022-03-02 14:56:20 +0100

    cors installed and imported

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,11 +2,14 @@ import express from "express"
 import listEndpoints from "express-list-endpoints"
 import authorsRouter from  "./services/authors/index.js"
 import articlesRouter from "./services/articles/index.js";
+import cors from "cors";
 
 const server = express()
 
 const port = 3001
 
+
+server.use(cors())
 server.use(express.json())
 
 server.use("/authors", authorsRouter)
